77e9f093dcd52a381698b7b64905704eb717747a
DESKTOP-AFB0PUS\Cloud <Cloud202511@outlook.com>
2025-08-18 21:29:00 +0200

    2

--- Controllers/JsonController.cs
+++ Controllers/JsonController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SummerizeMVC.Models;
 using System.Net;
+using System.Reflection;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace SummerizeMVC.Controllers
@@ -18,18 +19,34 @@ namespace SummerizeMVC.Controllers
         [HttpPost]
         public async Task<ActionResult> RequestTranscribeModel([FromBody]TranscribeModel model)
         {
+            var homeModel = new HomeModel();
+            if (string.IsNullOrEmpty(model.prompt)) return Json(homeModel);
             var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://b9xup5upy7.execute-api.eu-central-1.amazonaws.com/DEV/text");
             var test = Request;
             httpWebRequest.ContentType = "application/json";
             httpWebRequest.Method = "POST";
             List<string> badWords = new List<string>();
-            badWords.Add("naar mijn mening kunt u dus gewoon veilig op reis om uw dochter te bezoeken.");
-            badWords.Add("error: transcription error occurred: your request timed out because no new audio was received for 15 seconds.");
+            //badWords.Add("naar mijn mening kunt u dus gewoon veilig op reis om uw dochter te bezoeken.");
+            //badWords.Add("error: transcription error occurred: your request timed out because no new audio was received for 15 seconds.");
+            badWords.Add("Error: Transcription error occurred: Your request timed out because no new audio was received for 15 seconds.");
             foreach (var item in badWords)
             {
-                model.prompt = model.prompt.ToLower().Replace(item, "");
+                model.prompt = model.prompt.Replace(item, "");
             }
-            
+            BasicAWSCredentials credentials = new BasicAWSCredentials(aws_access_key_id, aws_secret_access_key);
+            //var request1 = new TranslateTextRequest
+            //{
+            //    SourceLanguageCode = "nl",
+            //    TargetLanguageCode = "en", 
+            //    Text = model.prompt
+            //};
+            //using (var client = new AmazonTranslateClient(credentials))
+            //{
+            //    var response = await client.TranslateTextAsync(request1);
+            //    model.prompt = response.TranslatedText;
+            //}
+            model.prompt = await TranslateNLtoEN(model.prompt, credentials);
+
             using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
             {
                 string json = "{\"modelId\":\"amazon.titan-text-express-v1\"," +
@@ -39,27 +56,62 @@ namespace SummerizeMVC.Controllers
 
                 streamWriter.Write(json);
             }
-            var homeModel = new HomeModel();
+            
             var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
             using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
             {
                 homeModel.Result = streamReader.ReadToEnd();
             }
 
-            var request = new TranslateTextRequest
+            homeModel.Result = await TranslateENtoNL(homeModel.Result, credentials);
+
+            //var request2 = new TranslateTextRequest
+            //{
+            //    SourceLanguageCode = "en",
+            //    TargetLanguageCode = "nl",
+            //    Text = homeModel.Result
+            //};
+
+            //using (var client = new AmazonTranslateClient(credentials))
+            //{                
+            //    var response = await client.TranslateTextAsync(request2);
+            //    homeModel.Result = response.TranslatedText;
+            //}
+            return Json(homeModel);
+        }
+
+        private async Task<string> TranslateNLtoEN(string text, BasicAWSCredentials credentials)
+        {
+            string result = "";
+            var request1 = new TranslateTextRequest
+            {
+                SourceLanguageCode = "nl",
+                TargetLanguageCode = "en",
+                Text = text
+            };
+            using (var client = new AmazonTranslateClient(credentials))
+            {
+                var response = await client.TranslateTextAsync(request1);
+                result = response.TranslatedText;
+            }
+            return result;
+        }
+
+        private async Task<string> TranslateENtoNL(string text, BasicAWSCredentials credentials)
+        {
+            string result = "";
+            var request1 = new TranslateTextRequest
             {
                 SourceLanguageCode = "en",
                 TargetLanguageCode = "nl",
-                Text = homeModel.Result
+                Text = text
             };
-            BasicAWSCredentials credentials = new BasicAWSCredentials(aws_access_key_id, aws_secret_access_key);
-
             using (var client = new AmazonTranslateClient(credentials))
-            {                
-                var response = await client.TranslateTextAsync(request);
-                homeModel.Result = response.TranslatedText;
+            {
+                var response = await client.TranslateTextAsync(request1);
+                result = response.TranslatedText;
             }
-            return Json(homeModel);
+            return result;
         }
 
 
